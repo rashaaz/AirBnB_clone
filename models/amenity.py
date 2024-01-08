d8f776e35922e0d74670c9ed78c9c6444827247e
randaaz <randaaziz972@gmail.com>
2024-01-08 09:42:00 -0800

    modfy

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Defines the Amenity class representing a
-type of amenity in the application
+type of amenity in the application.
 """
 
 from models.base_model import BaseModel
